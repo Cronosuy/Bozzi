647163a888d11da76704268b87b20cc8ffca5238
Alberto Rizzo <git.hubrizzo@gmail.com>
2023-01-18 20:16:17 -0300

    menuHamburguesa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 AOS.init();
 
-/****************************SLIDE************************************************************** */
+/**** MENU HAMBURGUESA **/
+const botonModalCentro = document.querySelector("#boton-modal-centro");
+const modalCentro = document.querySelector("#modal-centro");
+const cerrarModalCentro = document.querySelector("#cerrar-modal-centro");
+
+botonModalCentro.addEventListener("click", () => {
+  modalCentro.classList.add("active");
+});
+
+cerrarModalCentro.addEventListener("click", () => {
+  modalCentro.classList.remove("active");
+});
