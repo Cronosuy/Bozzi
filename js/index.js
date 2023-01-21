636f0e9e41f682d72c1439a9da12db5b21c00d9f
Alberto Rizzo <git.hubrizzo@gmail.com>
2023-01-21 16:27:35 -0300

    cambios4

--- js/index.js
+++ js/index.js
@@ -12,3 +12,81 @@ botonModalCentro.addEventListener("click", () => {
 cerrarModalCentro.addEventListener("click", () => {
   modalCentro.classList.remove("active");
 });
+
+const botonCurso1 = document.querySelector("#boton-curso1");
+const botonCurso2 = document.querySelector("#boton-curso2");
+const botonCurso3 = document.querySelector("#boton-curso3");
+const botonCurso4 = document.querySelector("#boton-curso4");
+
+let pagina;
+function abrirPagina() {
+  pagina = window.open("https://es-la.facebook.com/");
+}
+
+botonCurso1.addEventListener("click", () => {
+  Swal.fire({
+    color: "#fff",
+    title: "Curso 1",
+    text: "Do you want to continue",
+    icon: "info",
+    background: "#212121",
+    confirmButtonText: "Adquirir",
+    showCancelButton: true,
+    cancelButtonText: "Cerrar",
+    confirmButtonColor: "#008000",
+    cancelButtonColor: "#d33",
+    width: "900px",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      abrirPagina();
+    }
+  });
+});
+
+botonCurso2.addEventListener("click", () => {
+  Swal.fire({
+    color: "#fff",
+    title: "Curso 2",
+    text: "Do you want to continue",
+    icon: "info",
+    background: "#212121",
+    confirmButtonText: "Adquirir",
+    showCancelButton: true,
+    cancelButtonText: "Cerrar",
+    confirmButtonColor: "#008000",
+    cancelButtonColor: "#d33",
+    width: "900px",
+  });
+});
+
+botonCurso3.addEventListener("click", () => {
+  Swal.fire({
+    color: "#fff",
+    title: "Curso 3",
+    text: "Do you want to continue",
+    icon: "info",
+    background: "#212121",
+    confirmButtonText: "Adquirir",
+    showCancelButton: true,
+    cancelButtonText: "Cerrar",
+    confirmButtonColor: "#008000",
+    cancelButtonColor: "#d33",
+    width: "900px",
+  });
+});
+
+botonCurso4.addEventListener("click", () => {
+  Swal.fire({
+    color: "#fff",
+    title: "Curso 4",
+    text: "Do you want to continue",
+    icon: "info",
+    background: "#212121",
+    confirmButtonText: "Adquirir <a href=",
+    showCancelButton: true,
+    cancelButtonText: "Cerrar",
+    confirmButtonColor: "#008000",
+    cancelButtonColor: "#d33",
+    width: "900px",
+  });
+});
